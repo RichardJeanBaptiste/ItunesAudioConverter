2c37544dbbff73924d4e742d429468550da449f1
RichardJeanBaptiste <Richinbk1@gmail.com>
2020-08-15 00:25:38 -0400

    asdf

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def toMp3_message():
 
 # Create Album from audio files
 
-async def toAlbum(album,dir):
+def toAlbum(album,dir):
     try:
         #print("ZIP FILES " + album + " " + dir)
         albumDir = "zip/" + album
