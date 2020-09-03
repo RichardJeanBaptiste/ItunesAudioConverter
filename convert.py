6067e7483f999c8aaad49a2f16986cfc3bdfdddd
RichardJeanBaptiste <Richinbk1@gmail.com>
2020-09-03 14:24:03 -0400

    async build

--- convert.py
+++ convert.py
@@ -1,5 +1,58 @@
 import pafy 
 import ffmpeg
 import os
+import asyncio
+import shutil
+from mp3_tagger import MP3File
 
+
+# Download Audio
+async def changeAudio(x,dir,artist,album):
+    playlist = ""
+    try:
+        playlist = pafy.get_playlist(x)
+        for x in range(len(playlist) - 1 ):
+            playlist['items'][x]['pafy'].getbestaudio(preftype="m4a").download(filepath=dir)
+            print(x)
+    except OSError as e:
+        print(e)
+        return 'no video formats found...try again'
+    except IndexError as e:
+        print(e)
+    except Exception as e:
+        print(e)
+        return e
+    
+
+#asyncio.run(changeAudio("https://www.youtube.com/watch?v=xdGLK_sOWb0","a","a","a"))
+
+# convert files to mp3 format
+async def toMp3(artist,album,dir):
+    #toMp3_message()
+    try:
+        urls = os.listdir(dir)
+        for x in urls:
+            name = dir + "/" + x[:-5] + '.mp3'
+            print(name)
+            songUrl = dir + "/" + x
+            stream = ffmpeg.input(songUrl)
+            stream = ffmpeg.output(stream, name)
+            ffmpeg.run(stream)
+            mp3 = MP3File(name)
+            mp3.artist = artist
+            mp3.album = album
+            mp3.save()
+            os.remove(songUrl)
+    except Exception as e:
+        print(e)
+
+
+# Create zip directory 
+async def toAlbum(album,dir):
+    try:
+        albumDir = "zip/" + album
+        albumDir = album
+        shutil.make_archive(albumDir, 'zip', dir)
+    except Exception as e:
+        print(e)
 
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import os
 import uuid 
 import shutil
 import asyncio
-from flask import Flask, render_template, request, send_file
+from convert import *
+from flask import Flask, render_template, request, send_file, redirect, url_for
 from flask_socketio import SocketIO,emit, send
 from mp3_tagger import MP3File
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -32,84 +33,7 @@ sched.add_job(clearDirs,'interval',minutes=2)
 sched.start()
 '''
 
-
-# Download Audio
-def changeAudio(x,dir,artist,album):
-    changeAudio_message()
-    playlist = pafy.get_playlist(x)
-    for x in range(len(playlist) - 1):
-        try:
-            playlist['items'][x]['pafy'].getbestaudio(preftype="m4a").download(filepath=dir)
-        except OSError:
-            return 'no video formats found...try again'
-        except IndexError:
-            pass
-    #toMp3(artist,album,dir)
-
-
-
-def changeAudio_message():
-    try:
-        socketio.emit('changeAudio', 'Getting Url')
-    except Exception:
-        print(Exception)
-
-# Convert To Mp3
 urls = ""
-
-def toMp3(artist,album,dir):
-    #toMp3_message()
-    try:
-        urls = os.listdir(dir)
-        for x in urls:
-            name = dir + "/" + x[:-5] + '.mp3'
-            print(name)
-            songUrl = dir + "/" + x
-            stream = ffmpeg.input(songUrl)
-            stream = ffmpeg.output(stream, name)
-            ffmpeg.run(stream)
-            mp3 = MP3File(name)
-            mp3.artist = artist
-            mp3.album = album
-            mp3.save()
-            os.remove(songUrl)
-    except Exception as e:
-        print(e)
-
-
-def toMp3_message():
-    try:
-        socketio.emit('toMp3', 'To Mp3')
-    except Exception:
-        print(Exception)
-    
-
-
-# Create Album from audio files
-
-def toAlbum(album,dir):
-    try:
-        albumDir = "zip/" + album
-        albumDir = album
-        shutil.make_archive(albumDir, 'zip', dir)
-        toAlbum_message()
-    except Exception as e:
-        print(e)
-
-def toAlbum_message():
-    try:
-        socketio.emit('toAlbum', 'To Album')
-    except Exception:
-        print(Exception)
-    
-
-def startOver():
-    try:
-        socketio.emit('startOver', 'start again')
-    except Exception:
-        print(Exception)
-
-
 dir = ""
 album = ""
 
@@ -135,28 +59,29 @@ def getAudio():
     dir = "Dir-" + str(my_id)
     os.mkdir(dir)
 
-    changeAudio(url,dir,artist,album)
-    x = threading.Thread(target=toMp3,args=(artist,album,dir),daemon=True)
-    x.start()
-    #toMp3(artist,album,dir)
-    #y = threading.Thread(target=toAlbum,args=(album,dir),daemon=True)
-    #y.start()
-    #print(os.listdir(os.getcwd()))
     
+    asyncio.run(changeAudio(url,dir,artist,album))
+    
+    return redirect(url_for('mp3' , artist=artist, album=album, dir=dir))
 
-    x.join()
-    #y.join()
-    
-    if(x.isAlive):
-        print("still alive")
-    else:
-        toAlbum(album,dir)
-        zipFile = album + ".zip"
-        return send_file(zipFile) 
-    #return 
-     
-    
 
+@app.route("/mp3/<artist>/<album>/<dir>")
+def mp3(artist,album,dir):
+    asyncio.run(toMp3(artist,album,dir))
+    return redirect(url_for('zipAlbum', album=album, dir=dir))
+
+
+@app.route("/zip/<album>/<dir>")
+def zipAlbum(album,dir):
+    asyncio.run(toAlbum(album,dir))
+    return redirect(url_for('sendZip', album=album))
+
+@app.route("/sendfile/<album>")
+def sendZip(album):
+    zipFile = album + ".zip"
+    return send_file(zipFile)
+    
 
 if __name__ == "__main__":
     app.run()
+
